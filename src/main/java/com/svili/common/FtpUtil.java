cbdad5d88fab8e789a6ae6e1dca06d26594b5365
svili <153955424@qq.com>
2017-04-22 17:02:22 +0800

    Util for http ftp

---
+++ src/main/java/com/svili/common/FtpUtil.java
@@ -0,0 +1,164 @@
+package com.svili.common;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+/**
+ * FTP上传/下载
+ * 
+ * @author lishiwei
+ * @data 2017年4月18日
+ *
+ */
+public class FtpUtil {
+	/**
+	 * 上传文件至FTP服务器</br>
+	 * 此方法不会关闭输入流(input)
+	 * 
+	 * @param host
+	 *            FTP服务器hostname
+	 * @param port
+	 *            FTP服务器端口,默认21
+	 * @param username
+	 *            FTP登录账号
+	 * @param password
+	 *            FTP登录密码
+	 * @param filePath
+	 *            FTP服务器文件存放路径。例如分日期存放：/2015/01/01.可为空,空值表示根目录
+	 * @param filename
+	 *            上传到FTP服务器上的文件名,不能为空
+	 * @param input
+	 *            输入流
+	 * @return 成功返回true，否则返回false
+	 */
+	public static boolean uploadFile(String host, int port, String username, String password, String filePath,
+			String filename, InputStream input) {
+		boolean result = false;
+		FTPClient ftp = new FTPClient();
+		try {
+			// 连接FTP服务器
+			// 如果采用默认端口，可以使用ftp.connect(host)的方式直接连接FTP服务器
+			ftp.connect(host, port);
+			ftp.login(username, password);
+			int reply = ftp.getReplyCode();
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				throw new Exception("failed to get ftp connection ");
+			}
+			if (filePath == null || filePath.trim().equals("")) {
+				filePath = "/";
+			}
+			// 切换到上传目录
+			if (!ftp.changeWorkingDirectory(filePath)) {
+				// 如果目录不存在创建目录
+				String[] dirs = filePath.split("/");
+				String path = "";
+				for (String dir : dirs) {
+					if (null == dir || "".equals(dir)) {
+						continue;
+					}
+					path += "/" + dir;
+					if (!ftp.changeWorkingDirectory(path)) {
+						if (!ftp.makeDirectory(path)) {
+							ftp.disconnect();
+							throw new Exception("create dir erro : " + path);
+						}
+					}
+				}
+			}
+			// 设置上传文件的类型为二进制类型
+			ftp.setFileType(FTP.BINARY_FILE_TYPE);
+			//文件名不能为空
+			if(filename == null || filename.trim().equals("")){
+				throw new Exception("fileName is not allow to null ");
+			}
+			// 上传文件
+			if (ftp.storeFile(filename, input)) {
+				result = true;
+			} else {
+				ftp.disconnect();
+				throw new Exception("failed to upload file ");
+			}
+			ftp.logout();
+		} catch (Exception e) {
+			LogUtil.info("", e);
+		} finally {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException e) {
+					LogUtil.info("ftp disconnect erro", e);
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Description: 从FTP服务器下载文件
+	 * 
+	 * @param host
+	 *            FTP服务器hostname
+	 * @param port
+	 *            FTP服务器端口
+	 * @param username
+	 *            FTP登录账号
+	 * @param password
+	 *            FTP登录密码
+	 * @param remotePath
+	 *            FTP服务器上的相对路径
+	 * @param fileName
+	 *            要下载的文件名
+	 * @param localPath
+	 *            下载后保存到本地的路径
+	 * @return
+	 */
+	public static boolean downloadFile(String host, int port, String username, String password, String remotePath,
+			String fileName, String localPath) {
+		boolean result = false;
+		FTPClient ftp = new FTPClient();
+		try {
+			int reply;
+			ftp.connect(host, port);
+			// 如果采用默认端口，可以使用ftp.connect(host)的方式直接连接FTP服务器
+			ftp.login(username, password);// 登录
+			reply = ftp.getReplyCode();
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				return result;
+			}
+			ftp.changeWorkingDirectory(remotePath);// 转移到FTP服务器目录
+			FTPFile[] fs = ftp.listFiles();
+			for (FTPFile ff : fs) {
+				if (ff.getName().equals(fileName)) {
+					File localFile = new File(localPath + "/" + ff.getName());
+
+					OutputStream is = new FileOutputStream(localFile);
+					ftp.retrieveFile(ff.getName(), is);
+					is.close();
+				}
+			}
+
+			ftp.logout();
+			result = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException ioe) {
+				}
+			}
+		}
+		return result;
+	}
+}
---
+++ src/main/java/com/svili/common/HttpClientUtil.java
@@ -0,0 +1,226 @@
+package com.svili.common;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * HttpClient工具类</br>
+ * HTTP GET/POST
+ * 
+ * @author svili
+ * @data 2017年4月22日
+ *
+ */
+public class HttpClientUtil {
+
+	/**
+	 * create HttpClient
+	 * 
+	 */
+	private static CloseableHttpClient buildHttpClient() {
+		return HttpClients.createDefault();
+	}
+
+	/**
+	 * httpGet
+	 * 
+	 * @param uri
+	 *            for example,http://www.baidu.com or
+	 *            http://www.baidu.com?param_1=value...
+	 * @return response
+	 */
+	public static String sendGet(String uri) throws Exception {
+		return sendGet(uri, null, null);
+	}
+
+	/**
+	 * httpGet
+	 */
+	public static String sendGet(String url, Map<String, Object> params) throws Exception {
+		return sendGet(url, params, null);
+	}
+
+	/**
+	 * 
+	 * params.key:param name,if key is null or key equals(""),this key and value
+	 * will not append to request uri</br>
+	 * params.value:param value,if value is null, the value will not append to
+	 * request uri;if value is not null,append to uri with value.toString()</br>
+	 * header.value:if the value is null,it will be cast to String with
+	 * "null".</br>
+	 * 
+	 */
+	public static String sendGet(String url, Map<String, Object> params, Map<String, Object> headers) throws Exception {
+		String uri = url;
+		if (params != null && !params.isEmpty()) {
+			StringBuilder paramURL = new StringBuilder();
+			for (Entry<String, Object> entry : params.entrySet()) {
+				String name = entry.getKey();
+				Object value = entry.getValue();
+				if (name != null && !name.equals("")) {
+					paramURL.append("&");
+					paramURL.append(name).append("=");
+					if (value != null) {
+						paramURL.append(URLEncoder.encode(value.toString(), "UTF-8"));
+					}
+				}
+			}
+			uri = url + "?" + paramURL.substring(1);
+		}
+
+		HttpGet httpGet = new HttpGet(uri);
+
+		if (headers != null && !headers.isEmpty()) {
+			for (Entry<String, Object> entry : headers.entrySet()) {
+				httpGet.addHeader(entry.getKey(), String.valueOf(entry.getValue()));
+			}
+		}
+
+		return doRequest(httpGet);
+	}
+
+	public static String sendPost(String uri) throws Exception {
+		return sendPost(uri, null, null);
+	}
+
+	public static String sendPost(String url, Map<String, Object> params) throws Exception {
+		return sendPost(url, params, null);
+	}
+
+	/**
+	 * header.value:if the value is null,it will be cast to String with
+	 * "null".</br>
+	 */
+	public static String sendPost(String url, Map<String, Object> params, Map<String, Object> headers)
+			throws Exception {
+		HttpPost httpPost = new HttpPost(url);
+		if (params != null || !params.isEmpty()) {
+			// 创建参数列表
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(params.size());
+			for (Entry<String, Object> entry : params.entrySet()) {
+				String name = entry.getKey();
+				Object value = entry.getValue();
+				if (name != null || !name.equals("")) {
+					if (value != null) {
+						nameValuePairs.add(new BasicNameValuePair(name, value.toString()));
+					} else {
+						nameValuePairs.add(new BasicNameValuePair(name, null));
+					}
+				}
+			}
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+		}
+
+		if (headers != null && !headers.isEmpty()) {
+			for (Entry<String, Object> entry : headers.entrySet()) {
+				httpPost.addHeader(entry.getKey(), String.valueOf(entry.getValue()));
+			}
+		}
+
+		return doRequest(httpPost);
+	}
+
+	private static String doRequest(HttpUriRequest request) throws Exception {
+		CloseableHttpClient httpClient = buildHttpClient();
+		CloseableHttpResponse response = null;
+
+		try {
+			response = httpClient.execute(request);
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			if (httpClient != null) {
+				httpClient.close();
+			}
+		}
+		// 状态码
+		// int statusCode = response.getStatusLine().getStatusCode();
+		String result = "";
+
+		try {
+			result = EntityUtils.toString(response.getEntity(), "UTF-8");
+		} catch (ParseException | IOException e) {
+			throw e;
+		} finally {
+			if (response != null) {
+				response.close();
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * 解析Http Response响应头
+	 * 
+	 * @param response
+	 * @return
+	 */
+	private static Map<String, String> parseHeader(HttpResponse response) {
+		Header[] headers = response.getAllHeaders();
+
+		if (headers != null) {
+			Map<String, String> map = new HashMap<String, String>(headers.length);
+
+			for (Header header : headers) {
+				map.put(header.getName(), header.getValue());
+			}
+
+			// 不包括Cookie
+			map.remove("Set-Cookie");
+
+			return map;
+		}
+
+		return null;
+	}
+
+	/**
+	 * 解析Http Response响应Cookie
+	 * 
+	 * @param response
+	 * @return
+	 */
+	private static Map<String, String> parseCookie(HttpResponse response) {
+		Header[] cookies = response.getHeaders("Set-Cookie");
+
+		if (cookies != null) {
+			Map<String, String> map = new HashMap<String, String>(cookies.length);
+
+			for (Header header : cookies) {
+				String cookie = header.getValue().substring(0, header.getValue().indexOf(";"));
+				String[] kv = cookie.split("=");
+
+				if (kv.length == 2) {
+					map.put(kv[0].trim(), kv[1].trim());
+				} else {
+					map.put(kv[0].trim(), "");
+				}
+			}
+
+			return map;
+		}
+
+		return null;
+	}
+
+}
